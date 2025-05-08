6ee7553c73ec8ef0155e9e65788b7f8aa7ab6168
Kirill Tribunsky <tribunsky.kir@yandex.ru>
2025-05-08 17:09:04 +0800

    fix: oauth_cb -> oauth_callback

--- wunderkafka/callbacks/auth/iam.py
+++ wunderkafka/callbacks/auth/iam.py
@@ -11,7 +11,7 @@ from wunderkafka.logger import logger
 #   "The format is implementation-dependent and must be parsed accordingly"
 # so we can't annotate `_` with something meaningful.
 # we just don't use it for now
-def oauth_cb(_: Any) -> tuple[str, int]:  # noqa: ANN401
+def oauth_callback(_: Any) -> tuple[str, int]:  # noqa: ANN401
     region = os.getenv("AWS_REGION") or os.getenv("AWS_DEFAULT_REGION")
     if not region:
         msg = "Region not set - export AWS_REGION or AWS_DEFAULT_REGION before running."
